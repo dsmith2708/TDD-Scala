1e592c72f96829b627bc461c8f4e5f3f5c749a82
Daniel Smith <39262851+dsmith2708@users.noreply.github.com>
2019-08-06 15:46:59 +0100

    added more tests and satisfied requirements

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,4 +2,13 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
+
+  def fizzBuzz(x: Int): String = {
+    x match {
+      case 0 =>"0"
+      case 1 =>"1"
+      case 2 =>"2"
+    }
+  }
+
 }
--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -2,9 +2,21 @@ import collection.mutable.Stack
 import org.scalatest._
 
 class MainSpec extends WordSpec with Matchers {
+
   "calling fizzbuzz with value 0" should {
     "return string 0" in {
-      assert(Main.fizzbuzz(0) === "0")
+      Main.fizzBuzz(0) shouldBe "0"
+    }
+  }
+
+  "calling fizzbuzz with value 0" should {
+    "return string 1" in {
+      Main.fizzBuzz(1) shouldBe "1"
+    }
+  }
+  "calling fizzbuzz with value 2" should {
+    "return string 2" in {
+      Main.fizzBuzz(2) shouldBe "2"
     }
   }
 }
